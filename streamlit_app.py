fc896b47af5fc902ff6bcca04974895b3a5bf362
netj22693 <netolicka.jan@email.cz>
2025-05-21 12:03:25 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,36 @@
+# streamlit_app.py
 import streamlit as st
 import sqlalchemy
 
-# Create the SQL connection to pets_db as specified in your secrets file.
-conn = st.connection('pets_db', type='sql')
 
-# Query and display the data you inserted
-#pet_owners = conn.query('select * from pet_owners')
-#st.dataframe(pet_owners)
+import pymysql as mys
+import pandas as pd
+
+# já jsem si to ještě profesionálně obalil do try a except, abych viděl, zda se povedlo nebo ne
+
+# POZOR - pokud se připojuju na DB, musím být mimo VPN 
+pripojeni_na_DB = mys.connect(
+    host= "mysql57.r2.websupport.sk", 
+    port = 3311,
+    database="Kurz_SQL",
+    password= "",
+    user= "Kurz_SQL"
+        )
+st.write("All good - complete")
+
+df_zamestnanci_all = pd.read_sql_query("select * from EMP", con=pripojeni_na_DB)
+st.write(df_zamestnanci_all)
+
+
+
+import streamlit as st
+
+host=st.secrets.db_credentials.host,
+user=st.secrets.db_credentials.user,
+password=st.secrets.db_credentials.password,
+db=st.secrets.db_credentials.database
+
+conn = st.connection('db_credentials', type='sql')
+pet_owners = conn.query('select * from EMP')
+st.dataframe(pet_owners)
+
