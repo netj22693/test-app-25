e69687e1e62c947630122d4f1eca5eada9b71bff
netj22693 <netolicka.jan@email.cz>
2025-05-21 10:16:24 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import sqlalchemy
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Create the SQL connection to pets_db as specified in your secrets file.
+conn = st.connection('pets_db', type='sql')
+
+# Query and display the data you inserted
+#pet_owners = conn.query('select * from pet_owners')
+#st.dataframe(pet_owners)
