b9cc5c9d5e170758c15f2bed6cb4f33a0304eec9
netj22693 <netolicka.jan@email.cz>
2025-10-24 08:37:59 +0000

    test DB

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,26 @@ import streamlit as st
 
 with st.expander("Test"):
     st.write("Test")
+
+
+import streamlit as st
+import mysql.connector as mys
+import pandas as pd
+
+# Načtení údajů z secrets.toml
+db = st.secrets["mysql"]
+
+# Připojení k DB
+connection = mys.connect(
+    host=db["host"],
+    port=db["port"],
+    database=db["database"],
+    user=db["user"],
+    password=db["password"]
+)
+
+if connection.is_connected():
+    st.success("✅ Připojeno k databázi!")
+
+df_zamestnanci = pd.read_sql_query("select * from EMP where JOB LIKE '%C%'", con=connection)
+st.write(df_zamestnanci)
